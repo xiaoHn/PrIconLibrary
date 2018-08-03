c5dd35569d055466bc145b121c73c8017e3910b5
maw <1787364567@qq.com>
2018-08-03 14:04:36 +0800

    添加图片 尝试展示

---
+++ app/src/main/java/com/slient/m/priconlibrary/IconActivity.java
@@ -0,0 +1,13 @@
+package com.slient.m.priconlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IconActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_icon);
+    }
+}
